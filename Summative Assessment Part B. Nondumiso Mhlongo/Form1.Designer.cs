966246af234f6410ca0a9c15fc41da8a45882b29
Academy-User <Academy-User@DESKTOP-1L3B0TP>
2024-02-02 17:26:38 +0200

    Add project files.

---
+++ Summative Assessment Part B. Nondumiso Mhlongo/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Summative_Assessment_Part_B.Nondumiso_Mhlongo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(211, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a new word:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(170, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(315, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(256, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add word";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(274, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "OR";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 26);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Select a word:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(471, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(211, 26);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Select another word:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Hello",
+            "Bye"});
+            this.comboBox1.Location = new System.Drawing.Point(18, 269);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(145, 28);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Hello",
+            "Bye"});
+            this.comboBox2.Location = new System.Drawing.Point(476, 269);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(187, 28);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(18, 304);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(93, 24);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Remove";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(476, 304);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(93, 24);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Remove";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(256, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 53);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Contenate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(-3, 409);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(732, 53);
+            this.label5.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(728, 462);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Summative Assessment Part B. Nondumiso Mhlongo/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Summative_Assessment_Part_B.Nondumiso_Mhlongo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string newWord =   textBox1.Text.Trim();
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Please enter a word.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (comboBox1.Items.Contains(textBox1.Text))
+            {
+                MessageBox.Show("The word has already been entered.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            comboBox1.Items.Add(textBox1.Text);
+            comboBox2.Items.Add(textBox1.Text);
+
+
+            textBox1.Clear();
+
+
+            comboBox1.SelectedItem = newWord;
+            comboBox2.SelectedItem = newWord;
+       
+            MessageBox.Show("The new word has been added.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String textBoxText = textBox1.Text;
+            if (radioButton1.Checked || radioButton2.Checked)
+            {
+                // Check if a word has been selected from a ComboBox control
+                if (comboBox2.SelectedItem == null && comboBox2.SelectedItem == null)
+                {
+                    // Display a MessageBox
+                    MessageBox.Show("Please select a word from a ComboBox control.");
+                    return;
+                }
+                // Remove the selected word from the corresponding ComboBox control.
+                else if ( comboBox1.SelectedItem != null)
+                {
+                    comboBox1.Items.Remove(comboBox1.SelectedItem);
+                }
+                else if (comboBox2.SelectedItem != null)
+                   
+                {
+                    comboBox2.Items.Remove(comboBox2.SelectedItem);
+                 
+                }
+                MessageBox.Show("The word has been removed.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+
+            }
+            else
+
+            {
+                if ( comboBox1.SelectedItem == null || comboBox2.SelectedItem == null)
+
+                {
+                    MessageBox.Show("Please select a word from both ComboBox controls.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (comboBox1.SelectedItem.ToString () == comboBox2.SelectedItem.ToString())
+                    
+                {
+                    MessageBox.Show("Please select different words from the ComboBox controls.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                 
+
+                string concatenatedWord = comboBox1.SelectedItem.ToString() + comboBox2.SelectedItem.ToString();
+
+                button2.Text = concatenatedWord;
+            }
+
+            string[] txtEnterWord = textBoxText.Split(' ');
+
+            if (txtEnterWord.Length > 1 && txtEnterWord[0] == txtEnterWord[1])
+            {
+                MessageBox.Show("The TextBox contains the same word twice.");
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                button2.Text = "Remove Item";
+            }
+            else
+            {
+                button2.Text = "Concatenate";
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                button2.Text = "Remove Item";
+            }
+            else
+            {
+                button2.Text = "Concatenate";
+            }
+        }
+    }
+    }
+
+
